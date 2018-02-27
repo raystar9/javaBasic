968bfdf4fb524f3cc06441035aeafb24b10e7ab9
raystar <raystar@raystar-PC>
2018-02-27 09:29:48 +0900

    Day15

--- Java/src/week4/day15/exam/_01_collection/StringArrayListMain.java
+++ Java/src/week4/day15/exam/_01_collection/StringArrayListMain.java
@@ -1,9 +1,19 @@
 package week4.day15.exam._01_collection;
 
+import java.util.ArrayList;
+
 class StringArrayListMain {
 
 	public static void main(String[] args) {
+		ArrayList<String> stringList = new ArrayList<String>();
 		
+		stringList.add("Apple");
+		stringList.add("Banana");
+		stringList.add("Pear");
+		
+		for (String string : stringList) {
+			System.out.println(string);
+		}
 
 	}
 
